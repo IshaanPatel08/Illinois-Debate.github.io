2f6b5439d8a89485325184020c6f1f55cdc04f0c
Illinois-Debate <illinoisdebate1@gmail.com>
2024-07-28 00:03:21 -0500

    made a universal dark-mode.js

---
+++ dark-mode.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleSwitch = document.querySelector("#toggle");
+  const currentTheme = localStorage.getItem("theme");
+
+  if (currentTheme) {
+    document.body.classList.add(currentTheme);
+    document
+      .querySelectorAll("section")
+      .forEach((section) => section.classList.add(currentTheme));
+    if (currentTheme === "dark-mode") {
+      toggleSwitch.checked = true;
+    }
+  }
+
+  toggleSwitch.addEventListener("change", () => {
+    if (toggleSwitch.checked) {
+      document.body.classList.add("dark-mode");
+      document
+        .querySelectorAll("section")
+        .forEach((section) => section.classList.add("dark-mode"));
+      localStorage.setItem("theme", "dark-mode");
+    } else {
+      document.body.classList.remove("dark-mode");
+      document
+        .querySelectorAll("section")
+        .forEach((section) => section.classList.remove("dark-mode"));
+      localStorage.setItem("theme", "light-mode");
+    }
+  });
+});
